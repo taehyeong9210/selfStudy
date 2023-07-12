1bc423292d0faccc08e70fd54b539dc541c2a9cb
taehyeong9210 <skytubu@naver.com>
2023-07-13 01:52:42 +0900

    23년 7월 13일

---
+++ Blog/JavaScript/01.js
@@ -0,0 +1,10 @@
+// 문자열
+// console.log('문자열이 긴 경우에는' + ' 문자열을' + ' 나눈 뒤' + ' 다시 합칩니다.');
+
+//숫자
+let a = '5';
+console.log(a);
+console.log(typeof(parseInt(a))); // parseInt는 정수형으로만 바뀜
+console.log(parseInt('3.14'));
+console.log(parseFloat('3.14')); // parseFloat은 실수형으로 바뀜
+
