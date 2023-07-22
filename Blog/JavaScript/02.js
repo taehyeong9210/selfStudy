efad0595e65beada274720128fa7a6f93eceda61
taehyeong9210 <skytubu@naver.com>
2023-07-22 12:12:04 +0900

    23년 7월 22일

---
+++ Blog/JavaScript/02.js
@@ -0,0 +1,36 @@
+// const A = [3,5,4,2];
+// const b = '4632';
+
+// indexOf 이해
+// A.forEach((number, Index) => {
+//     const text = b.indexOf(String(number));
+//     if (text > -1 ) {
+//         console.log(` text = ${text}, index = ${Index}`);       
+//     } 
+// }) 
+
+// forEach 이해
+// const array = [1, 3, 5, 7];
+// array.forEach((number , index) => {
+//     console.log(number, index);
+// })
+// for ( let i = 0; i < 4; i++) {
+//     console.log(array[i], i)
+// }
+
+// 피셔-에이츠 셔플 이해
+// const candidate = Array(45).fill().map((v, i) => i + 1);
+//         const shuffle = [];
+//         for ( let i = candidate.length; i >= 0; i--) {
+//             const random = Math.floor(Math.random()* candidate.length)
+//             const spliceArray = candidate.splice(random, 1);
+//             const value = spliceArray[0];
+//             shuffle.push(value);
+//             console.log(random);
+//         }
+
+// setTimeout
+setTimeout(()=>{
+    console.log('hello');
+},3500);
+       
