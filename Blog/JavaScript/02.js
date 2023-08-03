02530c4b650e607636c05d1320e912890b096393
taehyeong9210 <skytubu@naver.com>
2023-08-03 23:58:35 +0900

    23년8월3일

--- Blog/JavaScript/02.js
+++ Blog/JavaScript/02.js
@@ -82,7 +82,24 @@
 // const fn2 = fn();
 // fn2();
 
-let a = [1, 2, 3];
-let c = [4, 5];
-let b = a.concat(c)
-console.log(b);
+// let a = [1, 2, 3];
+// let c = [4, 5];
+// let b = a.concat(c)
+// console.log(b);
+
+function aaa () {
+    setTimeout(() => {
+        console.log('d');
+    }, 0);
+    console.log('c');
+}
+
+setTimeout(() => {
+    console.log('a');
+    aaa();
+}, 0);
+
+setTimeout(() => {
+    aaa();
+    console.log('b')
+}, 0);
