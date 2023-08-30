ec4d14a0fe912abc1a65bf470b834bd7f1bcb91c
taehyeong9210 <skytubu@naver.com>
2023-08-31 01:15:04 +0900

    23년 8월 31일

---
+++ html/Animation-examples-gh-pages/README.md
@@ -0,0 +1,16 @@
+# Web Animations API Examples
+
+This is a collection of [Web Animations](https://developer.mozilla.org/en-US/docs/Web/API/Web_Animations_API) demos. This API allows creation and control of animations through JavaScript.
+
+- [View demos online](https://mozdevs.github.io/Animation-examples/)
+- or [download](https://github.com/mozdevs/Animation-examples/archive/gh-pages.zip) this repository and open `index.html` to browse examples locally.
+
+## Requirements
+
+For the demos to work, you need a browser that supports the Web Animations API. In Firefox, it is available from **version 48**. For other browsers, check out this [support table](http://caniuse.com/#feat=web-animation).
+
+## Contribute
+
+If you would like to submit additional demos featuring more use cases, please create a pull request.
+
+For bugs and general discussion, use the [issue tracker](https://github.com/mozdevs/Animation-examples/issues).
---
+++ html/Animation-examples-gh-pages/main.js
@@ -0,0 +1,66 @@
+'use strict';
+
+
+// TODO: we need to display local content instead of the one at Github
+const GITHUB_RAW = 'https://raw.githubusercontent.com/mozdevs/Animation-examples/';
+function buildRawCodeURL(base) {
+    let path = base.replace(window.location.href, '');
+    return `${GITHUB_RAW}gh-pages/${path}`;
+}
+
+window.onload = function () {
+    let menu = document.querySelector('.sidebar');
+    let burger = document.querySelector('.burger');
+    let viewer = document.querySelector('.viewer-wrapper');
+    let links = Array.from(document.querySelectorAll('a[target=viewer]'));
+    let viewSourceLink = document.querySelector('.view-source a');
+    let viewSource = document.querySelector('.view-source');
+
+
+    // -------------------------------------------------------------------------
+    // handle View Source Code button
+    // -------------------------------------------------------------------------
+
+    links.forEach(function (link) {
+        link.addEventListener('click', function(event) {
+            // only update and show the button for demos
+            if (/demos\//.test(link.href)) {
+                viewSourceLink.href = buildRawCodeURL(link.href);
+                viewSource.style.display = 'block';
+            }
+            else {
+                viewSourceLink.href = '#';
+                viewSource.style.display = 'none';
+            }
+        });
+    });
+
+    // -------------------------------------------------------------------------
+    // handle responsive menu
+    // -------------------------------------------------------------------------
+
+    let hideMenu = function () {
+        viewer.classList.add('mobile-show');
+        viewer.classList.remove('mobile-hidden');
+
+        menu.classList.add('mobile-hidden');
+        menu.classList.remove('mobile-show');
+    };
+
+    let showMenu = function () {
+        viewer.classList.add('mobile-hidden');
+        viewer.classList.remove('mobile-show');
+
+        menu.classList.add('mobile-show');
+        menu.classList.remove('mobile-hidden');
+
+        viewSourceLink.href = '#';
+        viewSource.style.display = 'none';
+    };
+
+    links.forEach(function(link) {
+        link.addEventListener('click', hideMenu);
+    });
+
+    burger.querySelector('button').addEventListener('click', showMenu);
+};
